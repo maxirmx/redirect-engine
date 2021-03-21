8efdbb78898257d9c48e6cc1cceec64d1b847393
samsonov <maxirmx@MSS-WS-N.localdomain>
2021-03-21 09:54:01 +0300

    Arranging database scripts 3

--- db_scripts/db.upgrade.1.sql
+++ db_scripts/db.upgrade.1.sql
@@ -1,3 +1,9 @@
+-- Database upgrade #1
+-- maxirmx
+-- March 14, 2021
+-- Added country_iso, referer and user agent
+
+
 ALTER TABLE public.clicks
     ADD COLUMN country_iso character varying(2) COLLATE pg_catalog."default";
 
